f87ceb56273869869b9f846baa0405788baef01a
kaoniikura88 <sapozhnikova_anastasia@mail.ru>
2021-03-16 00:14:52 +0300

    init: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let braces = [];
+
+    for(let i=0; i<bracketsConfig.length; i++) {
+        braces.push(bracketsConfig[i].join(''))
+    }
+    for(let i=0; i<braces.length; i++){
+        if(str.includes(braces[i])){
+            str = str.replace(braces[i], "");
+            i = -1;
+        }
+    }
+    return str.length === 0;
 }
+
